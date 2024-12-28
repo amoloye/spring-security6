053e139080ab5cf7fca3937e31abd3695168ff11
amoloye <adedamolaadegoke2@gmail.com>
2024-12-28 12:10:17 +0200

    Custom UserDetailsService and InMemoryUserDetailsManager with bypassing password Encoder with {noop}

---
+++ src/main/java/org/example/springsecurity6/controller/ProductController.java
@@ -0,0 +1,31 @@
+package org.example.springsecurity6.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private record Product(Integer productId, String productName, double price){}
+        List<Product> products = new ArrayList<>(
+                List.of(
+                        new Product(1,"iphone",999.99),
+                        new Product(2, "Mac Pro", 2099.99)
+                )
+
+        );
+
+    @GetMapping
+    public List<Product> getProducts(){
+        return products;
+    }
+
+    @PostMapping
+    public Product saveproduct(@RequestBody Product product){
+        products.add(product);
+        return product;
+    }
+}
